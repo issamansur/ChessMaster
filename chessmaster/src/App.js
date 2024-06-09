b0cd65772ccaca8b1c21781a20ffcd93ca9660a6
User#None <77580844+issamansur@users.noreply.github.com>
2024-06-09 23:29:16 +0000

    Added `react-router-dom`

--- chessmaster/src/App.js
+++ chessmaster/src/App.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
 import GamePage from "./pages/GamePage";
 
 import './App.css';
-import Header from "./components/Header";
-
 
 function App() {
 
   return (
     <div className="App">
       <Header onSearch={(text) => console.log(text)} />
-      <GamePage />
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/games/:gameid" element={<GamePage />} />
+          
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
+//<Route path="/" element={<MainPage />} />
+//<Route path="/users/:userid" element={<UserPage />} />
 export default App;
