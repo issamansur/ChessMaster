0addd27f84bdbb41a16d612decc8ee4653b4b12d
Issa <issa_mansur@mail.ru>
2024-06-24 15:19:38 +0300

    Removed logic with `chess.js` in `Board`

--- chessmaster/src/components/Board.js
+++ chessmaster/src/components/Board.js
@@ -1,54 +1,30 @@
-import { useEffect, useState } from "react";
-
-import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
 
 const Board = (props) => {
-    const { fen, onMove } = props;
-    const [game, setGame] = useState(null);
+    const { 
+        fen, 
+        boardOrientation, 
+        arePiecesDraggable, 
+        onMove 
+    } = props;
 
-    useEffect(() => {
-        console.log(fen);
-        setGame(new Chess(fen));
-    }, [fen]);
-
-    function boardMove(from, to) {
+    async function boardMove(from, to, piece) {
         const move = {
+            figure: piece[0] == "w" ? piece[1].toUpperCase() : piece[1].toLowerCase(),
             from: from,
             to: to,
-            promotion: "q",
         };
 
-        try {
-            game.move(move);
-            const lastMove = getLastMove(game);
-            setGame(new Chess(game.fen()));
-            onMove(lastMove);
-        } catch (error) {
-            return false;
-        }
-    }
-
-    function getLastMove(game) {
-        const history = game.history({ verbose: true });
-        const lastMove = history[history.length - 1];
-
-        let move = {
-            figure: lastMove.color === "w" ? 
-                lastMove.piece.toUpperCase() : lastMove.piece,
-            from: lastMove.from,
-            to: lastMove.to,
-            promotion: lastMove.promotion ?? null,
-        };
-
-        return move;
+        return await onMove(move);
     }
 
     return (
         <Chessboard 
-            position={game?.fen()} 
-            onPieceDrop={boardMove}
+            position={fen} 
+            boardOrientation={boardOrientation}
+            arePiecesDraggable={arePiecesDraggable}
+            onPieceDrop={(from, to, piece) => boardMove(from, to, piece)}
         />
     )
 };
