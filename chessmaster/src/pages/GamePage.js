7335b322a9cc573f26d152be26a8b9f3e5f960b5
Issa <issa_mansur@mail.ru>
2024-06-24 15:28:15 +0300

    Added await process across game

--- chessmaster/src/pages/GamePage.js
+++ chessmaster/src/pages/GamePage.js
@@ -17,17 +17,61 @@ const GamePage = (props) => {
     const { gameid } = useParams();
 
     const [game, setGame] = useState(null);
+    const [players, setPlayers] = useState({
+        whitePlayerName: null,
+        blackPlayerName: null,
+    });
+
     const apiManager = () => new ApiManager(user.token);
 
+    const isUserPlaying = () => user?.id === game?.blackPlayerId || user?.id === game?.whitePlayerId;
+
+    const isWhiteMove = game?.fen.split(" ")[1] === "w";
+
+    const pollGameUpdates = async (currentFen) => {
+        console.log("Polling game updates");
+
+        const intervalId = setInterval(async () => {
+            const _game = await apiManager().getGame(gameid);
+            if (_game && _game.fen !== currentFen) {
+                console.log("Game updated");
+                setGame(_game);
+                if (!isUserPlaying()) {
+                    currentFen = _game.fen;
+                }
+                else {
+                    clearInterval(intervalId);
+                }
+            }
+        }, 100);
+
+        return () => clearInterval(intervalId);
+    };
+
     async function moveHandler(move) {
-        if (user?.id !== game.blackPlayerId && user?.id !== game.whitePlayerId) {
+        if (!isUserPlaying()) {
             return;
         }
 
-        const _game = await apiManager.moveGame(gameid, move);
-        if (_game.fen != game.fen) {
-            setGame(_game);
+        if (game.whitePlayerId === user.id && /^[a-z]/.test(move.figure)) {
+            return;
         }
+
+        if (game.blackPlayerId === user.id && /^[A-Z]/.test(move.figure)) {
+            return;
+        }
+
+        const _move = move.figure + move.from + move.to + (move.promotion ? move.promotion : "");
+
+        const _game = await apiManager().moveGame(gameid, _move);
+
+        if (_game.message) {
+            return false;
+        }
+        
+        setGame(_game);
+        pollGameUpdates(_game.fen);
+        return true;
     }
 
     function playerClickHandler(username) {
@@ -36,23 +80,41 @@ const GamePage = (props) => {
 
     useEffect(() => {
         async function getGame() {
-            let _game = await apiManager.getGame(gameid);
-            if (_game === null) {
-                return;
+            let _game = await apiManager().getGame(gameid);
+            
+            if (!_game?.fen) {
+                return null;
             }
-            const _user1 = await apiManager.getUser(_game.blackPlayerId);
-            const _user2 = await apiManager.getUser(_game.whitePlayerId);
-            _game = {
-                ..._game,
-                blackPlayerName: _user1.username,
-                whitePlayerName: _user2.username,
-            };
-
+            
+            const _user1 = await apiManager().getUser(_game.whitePlayerId);
+            const _user2 = await apiManager().getUser(_game.blackPlayerId);
+    
+            setPlayers({
+                whitePlayerName: _user1.username,
+                blackPlayerName: _user2.username,
+            });
             setGame(_game);
+        return _game;
         }
 
-        getGame();
-    }, [gameid]);
+        getGame().then(
+            (_game) =>
+            {
+                if (!_game?.fen) {
+                    return null;
+                }
+
+                const _isWhiteMove = _game.fen.split(" ")[1] === "w";
+                if (
+                    (user?.id !== _game.blackPlayerId && user?.id !== _game.whitePlayerId) ||
+                    (_isWhiteMove && user.id === _game.blackPlayerId) ||
+                    (!_isWhiteMove && user.id === _game.whitePlayerId)
+                ) {
+                    pollGameUpdates(_game.fen);
+                }
+            }
+        );
+    }, []);
 
     if (game?.fen === undefined) {
         return null;
@@ -63,16 +125,27 @@ const GamePage = (props) => {
             <div className="game-container">
                 <UserBadge 
                     position="left"
-                    username={game?.blackPlayerName}
+                    username={
+                        game.blackPlayerId === user?.id ? 
+                        players.whitePlayerName : players.blackPlayerName 
+                    }
                     avatar={true}
                     onClick={playerClickHandler}
                 />
                 <div className="board-container">
-                    <Board fen={game?.fen} onMove={async (move) => moveHandler(move)} />
+                    <Board 
+                        fen={game.fen} 
+                        boardOrientation={game.blackPlayerId === user?.id ? "black" : "white"} 
+                        arePiecesDraggable={game.whitePlayerId === user?.id || game.blackPlayerId === user?.id}
+                        onMove={async (move) => await moveHandler(move)} 
+                    />
                 </div>
                 <UserBadge 
                     position="right"
-                    username={game?.whitePlayerName}
+                    username={
+                        game.blackPlayerId === user?.id ? 
+                        players.blackPlayerName : players.whitePlayerName 
+                    }
                     avatar={true}
                     onClick={playerClickHandler} 
                 />
